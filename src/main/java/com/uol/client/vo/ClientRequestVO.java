589bb968106544b3d9237593ce8e4dfbc8ee7f29
mauricio-melo <mauriciomelo.contato@outlook.com>
2019-05-28 23:36:58 -0300

    Ajuste Swagger VO

--- src/main/java/com/uol/client/vo/ClientRequestVO.java
+++ src/main/java/com/uol/client/vo/ClientRequestVO.java
@@ -1,5 +1,6 @@
 package com.uol.client.vo;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ClientRequestVO {
+
+    @ApiModelProperty(notes = "Name's client", example = "Jose Silva", required = true, position = 1)
     private String name;
+
+    @ApiModelProperty(notes = "Age's client", example = "25", required = true, position = 2)
     private Integer age;
 }
